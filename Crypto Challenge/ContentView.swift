1865e2446236ff0af29bd9b138889435d28d9e65
Adrian Talbot <adrianrtalbot@me.com>
2024-08-24 11:34:21 +0100

    Add .gitignore with rules for XCode and Swift language. Add rule to exclude Config.xcconfig from source control Add Config.xcconfig and inert line to define API_KEY Add configuration for Config file to debug and run targets Add key API_KEY to bundle

--- Crypto Challenge/ContentView.swift
+++ Crypto Challenge/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var apiKey: String? = "None"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Your API key is \n \(apiKey ?? "empty")")
         }
         .padding()
+        .onAppear {
+            apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+        }
     }
 }
 
