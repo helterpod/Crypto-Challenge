af31cd24bd359f48ec022068d35d57c0441b5f40
Adrian Talbot <adrianrtalbot@me.com>
2024-08-24 11:08:48 +0100

    Initial Commit

---
+++ Crypto Challenge/Crypto_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_ChallengeApp.swift
+//  Crypto Challenge
+//
+//  Created by Adrian Richard Talbot on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
